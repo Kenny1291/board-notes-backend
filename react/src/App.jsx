f814d39de66ba390f60c8556dd22b7199d80523a
Raiquen <raiqueng@live.com>
2023-04-11 18:03:26 +0200

    note JSX component; render all via API call

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import Note from "./components/Note"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/notes')
+      .then(response => response.json())
+      .then(data => setNotes(data))
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+    {notes.map((note) => <Note note={note.content} key={note.id} />)}
+    </>
   )
 }
 
