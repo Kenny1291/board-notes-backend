354e6fcf1f272a32313a56b3fee35aa7e3cf0888
Raiquen <raiqueng@live.com>
2023-04-12 17:18:00 +0200

    sending PUT request only after user finished type

--- react/src/components/Note.jsx
+++ react/src/components/Note.jsx
@@ -1,7 +1,23 @@
-export default function Note({note}) {
+import { useCallback } from "react"
+import axiosClient from "../axios"
+import { debounce } from "lodash"
+
+export default function Note({note, onContentChange}) {
+
+    const sendUpdateRequest = useCallback(
+        debounce((id, newContent) => {
+        axiosClient.put(`/notes/${id}`, {content: newContent})
+      }, 1000),
+      [])
+
+    const updateContent = (e) => {
+        onContentChange(note.id, e.target.value)
+        sendUpdateRequest(note.id, e.target.value)
+    }
+      
     return (
         <div>
-            {note}
+            <input type="text" value={note.content} onChange={updateContent} />
         </div>
     )
 }
