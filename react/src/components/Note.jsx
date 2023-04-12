7a340fa25b9e2019f4377909a81970e8b294c8bd
Raiquen <raiqueng@live.com>
2023-04-12 19:00:07 +0200

    make note draggable with handle

--- react/src/components/Note.jsx
+++ react/src/components/Note.jsx
@@ -1,6 +1,7 @@
 import { useCallback } from "react"
 import axiosClient from "../axios"
 import { debounce } from "lodash"
+import Draggable from 'react-draggable'
 
 export default function Note({note, onContentChange}) {
 
@@ -16,6 +17,13 @@ export default function Note({note, onContentChange}) {
     }
       
     return (
-        <textarea className="bg-yellow-note m-4 p-5 min-h-[13rem] shadow-xl" type="text" value={note.content} onChange={updateContent} />
+        <Draggable
+            handle=".handle"
+        >
+            <div className="w-fit h-fit">
+                <div className="handle text-center mt-4 cursor-move"><span className="material-icons-round">drag_handle</span></div>
+                <textarea className="bg-yellow-note mx-4 mb-4 p-5 min-h-[13rem] shadow-xl resize" type="text" value={note.content} onChange={updateContent} />
+            </div>
+        </Draggable>
     )
 }
