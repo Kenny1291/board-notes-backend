4f8c9304e80865b24b729da19e1eecbbc809031e
Raiquen <raiqueng@live.com>
2023-04-14 17:51:25 +0200

    testing api route without prefix for Vercel

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::apiResource('/notes', NoteController::class);
+Route::apiResource('notes', NoteController::class);
